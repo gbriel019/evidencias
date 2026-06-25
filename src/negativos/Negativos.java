09c948d354934ab10b6d23196e12b966cbcbbb4f
bielh <gabehpc@gmail.com>
2026-06-25 12:24:49 -0300

    Analisa os números digitados e retorna os números negativos

--- src/negativos/Negativos.java
+++ src/negativos/Negativos.java
@@ -3,7 +3,7 @@ package negativos;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class Negativos {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
