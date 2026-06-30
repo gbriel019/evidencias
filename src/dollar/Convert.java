7b422a8c18a8891d888afeaf1f90a364ad40f858
bielh <gabehpc@gmail.com>
2026-06-30 09:23:08 -0300

    Faz a conversão do dollar, a ser pago em um cambio em reais brasileiros, calculando a taxa do IOF de 6%

---
+++ src/dollar/Convert.java
@@ -0,0 +1,9 @@
+package dollar;
+
+public class Convert {
+    public double valueDollar;
+    public double quantity;
+
+
+    public double convertDollarToReais() { return (quantity * valueDollar) * 1.06;}
+}
---
+++ src/dollar/Program.java
@@ -0,0 +1,26 @@
+package dollar;
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        Convert convert = new Convert();
+        System.out.print("What is the dollar price? ");
+        convert.valueDollar = sc.nextDouble();
+
+        System.out.print("How many dollars will be bought? ");
+        convert.quantity = sc.nextDouble();
+
+        System.out.printf("Amount to be paid in reais: %.2f%n", convert.convertDollarToReais());
+
+        sc.close();
+
+    }
+}
